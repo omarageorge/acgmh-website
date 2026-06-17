347a02dc9558b32f9bb724307cd374fd9937f0c4
George Omara <georgeomara8@gmail.com>
2026-06-17 18:26:09 +0300

    feat: Add children to Conference Themes in navLinks for improved navigation structure

--- src/data/nav-links.ts
+++ src/data/nav-links.ts
@@ -3,7 +3,14 @@ import { NavLink } from './definitions';
 export const navLinks: NavLink[] = [
   { href: '/', label: 'Home' },
   { href: '/abstracts', label: 'Abstracts' },
-  { href: '/themes', label: 'Conference Themes' },
+  {
+    href: '/themes',
+    label: 'Conference Themes',
+    children: [
+      { href: '/themes', label: 'Major Themes' },
+      { href: '/themes/sub-themes', label: 'Sub Themes' },
+    ],
+  },
   { href: '/sponsor', label: 'Sponsor' },
   // { href: '/contact', label: 'Contact' },
 ];
